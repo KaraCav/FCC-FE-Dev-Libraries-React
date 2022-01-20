40a554c5d03600935f0e33b10cb8588ea048ff5a
Kat <karacav@msn.com>
2022-01-20 15:51:53 -0800

    First 2 answers

---
+++ part-1.jsx
@@ -0,0 +1,11 @@
+// 1.	Create a Simple JSX Element
+const JSX = <h1>Hello JSX!</h1>;
+
+// 2.	Create a Complex JSX Element and Add Comments 
+const JSX = (
+  <div>
+{/*myComment*/}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
