3a167a7b226918493ea4eff26925cc19805e42fa
Kat <karacav@msn.com>
2022-01-21 21:54:37 -0800

    Number 3-4

--- part-1.jsx
+++ part-1.jsx
@@ -9,3 +9,21 @@ const JSX = (
     <p>Here's a subtitle</p>
   </div>
 );
+
+// 3. Render HTML Elements to the DOM- grab the id 'challenge-node'
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+ReactDOM.render(JSX, document.getElementById('challenge-node'))
+
+// 4. Define an HTML Class in JSX
+// a click event in JSX is onClick, instead of onclick. onchange becomes onChange
+// class becomes className
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
